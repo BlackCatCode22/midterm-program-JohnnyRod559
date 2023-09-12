e51046aa31b1c131e890f1b5cd88d56cc606d08c
JohnnyRod559 <141672746+JohnnyRod559@users.noreply.github.com>
2023-09-12 11:58:05 -0700

    Start of midterm, incomplete

---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.runtime.SwitchBootstraps;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.*;
+import java.time.LocalDate;
+import java.time.Period;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+       /* Scanner scanner = new Scanner(System.in);
+
+        System.out.println(" Please enter your name: ");
+       String userName = scanner.nextLine();
+
+        System.out.println(" Welcome " + userName + " lets get started, welcome to my Zoo program! ");
+*/
+        String uniqueID;
+
+
+
+        }
+
+
+
+
+
+        }
+    }
